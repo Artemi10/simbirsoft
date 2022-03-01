502a3331bb8528c6e7a608108170c803b6f0dc56
lyaha <lyah.artem10@mailru>
2022-03-01 21:39:14 +0300

    implement remember-me

--- src/main/java/com/example/simbirsoft/configuration/SecurityConfig.java
+++ src/main/java/com/example/simbirsoft/configuration/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.simbirsoft.configuration;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,38 +18,50 @@ import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${remember-me.key}")
+    private String rememberMeKey;
     private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests -> authorizeRequests
+                .authorizeRequests(authorizeRequestsConfig -> authorizeRequestsConfig
                         .antMatchers("/", "/css/*").permitAll()
                         .antMatchers("/auth/*", "/auth/log-in/*", "/user").not().authenticated()
                         .anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin
+                .formLogin(formLoginConfig -> formLoginConfig
                         .defaultSuccessUrl("/")
                         .loginPage("/auth/log-in")
                         .failureUrl("/auth/log-in?error=true")
                         .usernameParameter("email")
                         .defaultSuccessUrl("/"))
-                .logout(logout -> logout
+                .logout(logoutConfig -> logoutConfig
                         .permitAll()
                         .invalidateHttpSession(true)
                         .clearAuthentication(true)
                         .deleteCookies("JSESSIONID")
                         .logoutUrl("/auth/log-out"))
+                .rememberMe(rememberMeConfig -> rememberMeConfig
+                        .key(rememberMeKey)
+                        .rememberMeCookieName("authorization")
+                        .useSecureCookie(true)
+                        .userDetailsService(userDetailsService))
                 .csrf(AbstractHttpConfigurer::disable);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationProvider daoAuthenticationProvider() {
         var provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(passwordEncoder());
         provider.setUserDetailsService(userDetailsService);
-        auth.authenticationProvider(provider);
+        return provider;
     }
 
     @Bean

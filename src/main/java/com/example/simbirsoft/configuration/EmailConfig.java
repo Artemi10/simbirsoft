b3ba389dbfd06c82b4a108ca43b432e47334eebb
lyaha <lyah.artem10@mailru>
2022-03-08 17:13:52 +0300

    refactoring

---
+++ src/main/java/com/example/simbirsoft/configuration/EmailConfig.java
@@ -0,0 +1,54 @@
+package com.example.simbirsoft.configuration;
+
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    private Properties emailProperties;
+    private Properties emailPropertiesConfig;
+
+    @PostConstruct
+    public void init() throws IOException {
+        emailProperties = loadProperties("email-properties/email.properties");
+        emailPropertiesConfig = loadProperties("email-properties/email-config.properties");
+    }
+
+    private Properties loadProperties(String path) throws IOException {
+        var properties = new Properties();
+        var inputStream = new ClassPathResource(path).getInputStream();
+        properties.load(inputStream);
+        return properties;
+    }
+
+    @Bean
+    @Lookup
+    @Scope("prototype")
+    public Message emailMessage(Session session) throws MessagingException {
+        var message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(emailProperties.getProperty("emailAddress")));
+        return message;
+    }
+
+    @Bean
+    protected Session session() {
+        return Session.getDefaultInstance(emailPropertiesConfig, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                var address = emailProperties.getProperty("emailAddress");
+                var password = emailProperties.getProperty("emailPassword");
+                return new PasswordAuthentication(address, password);
+            }
+        });
+    }
+}
--- src/main/java/com/example/simbirsoft/service/email/ResetEmailMessageSender.java
+++ src/main/java/com/example/simbirsoft/service/email/ResetEmailMessageSender.java
@@ -3,70 +3,33 @@ package com.example.simbirsoft.service.email;
 import com.example.simbirsoft.exception.EmailException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.IOException;
-import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
 public class ResetEmailMessageSender implements MessageSender {
     @Value("${server.domain.url}")
     private String domainURL;
-    private Properties emailProperties;
-    private Properties emailPropertiesConfig;
-
-    @PostConstruct
-    public void init() throws IOException {
-        emailProperties = loadProperties("email-properties/email.properties");
-        emailPropertiesConfig = loadProperties("email-properties/email-config.properties");
-    }
-
-    private Properties loadProperties(String path) throws IOException {
-        var properties = new Properties();
-        var inputStream = new ClassPathResource(path).getInputStream();
-        properties.load(inputStream);
-        return properties;
-    }
+    private final Message message;
 
     @Override
     public void sendMessage(String email, String content) {
         try{
-            var message = createMessage(email);
-            setContent(message, email, content);
+            setContent(email, content);
             Transport.send(message);
         } catch (MessagingException exception) {
             throw new EmailException("Не удалость отправить email");
         }
     }
 
-    protected void setContent(Message message, String email, String content) throws MessagingException {
+    protected void setContent(String email, String content) throws MessagingException {
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
         message.setSubject("Востановление пароля");
         var link = String.format("%s/user/update?email=%s&token=%s", domainURL, email, content);
         var text = String.format("Востановить пароль можно по ссылке: %s", link);
         message.setText(text);
-    }
-
-    private Message createMessage(String email) throws MessagingException {
-        var message = new MimeMessage(createSession());
-        message.setFrom(new InternetAddress(emailProperties.getProperty("emailAddress")));
-        message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
-        return message;
-    }
-
-    private Session createSession() {
-        return Session.getDefaultInstance(emailPropertiesConfig, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                var address = emailProperties.getProperty("emailAddress");
-                var password = emailProperties.getProperty("emailPassword");
-                return new PasswordAuthentication(address, password);
-            }
-        });
     }
 }

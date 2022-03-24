1002efc4e2a5589f21cf9d3f3c3e1f40a7952e8a
lyaha <lyah.artem10@mailru>
2022-03-24 15:46:27 +0300

    add model repository

---
+++ src/main/java/com/example/simbirsoft/repository/AppRepository.java
@@ -0,0 +1,37 @@
+package com.example.simbirsoft.repository;
+
+import com.example.simbirsoft.entity.App;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface AppRepository extends JpaRepository<App, Long> {
+    @Query("""
+              SELECT app
+              FROM App app
+              WHERE app.user.email = :email
+              ORDER BY app.id DESC""")
+    Page<App> findAllByUserEmail(String email, Pageable pageable);
+
+    @Query("""
+              SELECT count(app)
+              FROM App app
+              WHERE app.user.email = :email""")
+    int getUserAppsAmount(String email);
+
+    @Query("""
+              SELECT app
+              FROM App app
+              WHERE app.user.email = :email
+              AND app.id = :noteId""")
+    Optional<App> findUserAppById(long noteId, String email);
+
+    @Modifying
+    @Transactional
+    void deleteByIdAndUserEmail(long noteId, String email);
+}
--- src/main/java/com/example/simbirsoft/repository/ApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.simbirsoft.repository;
-
-import com.example.simbirsoft.entity.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
-}

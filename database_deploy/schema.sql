25a5b1cfd9764bedc1b97c6e2725e12d2709dca7
lyaha <lyah.artem10@mailru>
2022-03-25 19:30:07 +0300

    update schema

--- database_deploy/schema.sql
+++ database_deploy/schema.sql
@@ -1,14 +1,35 @@
 create table users
 (
-    id          bigserial,
-    email       varchar(254) not null,
-    password    varchar(254) not null,
+    id          bigserial
+        constraint users_pk
+            primary key,
+    email       varchar(255) not null,
+    password    varchar(255) not null,
     authority   varchar(6)   not null,
-    reset_token varchar(36)
+    reset_token varchar(128)
 );
 
 alter table users
     owner to postgres;
+
+create table applications
+(
+    id            bigserial
+        constraint applications_pk
+            primary key,
+    name          varchar(255) not null,
+    creation_time timestamp    not null,
+    user_id       bigint       not null
+        constraint applications_users_id_fk
+            references users
+            on delete cascade
+);
+
+alter table applications
+    owner to postgres;
+
+create unique index applications_id_uindex
+    on applications (id);
 
 create unique index users_email_uindex
     on users (email);
@@ -16,22 +37,22 @@ create unique index users_email_uindex
 create unique index users_id_uindex
     on users (id);
 
-create table notes
+create table events
 (
-    id            bigserial
-        constraint notes_pk
+    id                bigserial
+        constraint events_pk
             primary key,
-    title         varchar(40)  not null,
-    text          varchar(254) not null,
-    user_id       bigint       not null
-        constraint notes_users_id_fk
-            references users (id)
-            on update cascade on delete cascade,
-    creation_time timestamp    not null
+    name              varchar(50) not null,
+    extra_information varchar(255),
+    time              timestamp   not null,
+    application_id    bigint      not null
+        constraint events_applications_id_fk
+            references applications
+            on delete cascade
 );
 
-alter table notes
+alter table events
     owner to postgres;
 
-create unique index notes_id_uindex
-    on notes (id);
+create unique index events_id_uindex
+    on events (id);
